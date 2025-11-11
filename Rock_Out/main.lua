3102bf5f48c085f5fc92ee46d7108bdebeb7995e
coltbowlin0 <coltbowlin0@gmail.com>
2025-11-11 08:42:33 -0500

    Set Up the main Script

--- Rock_Out/main.lua
+++ Rock_Out/main.lua
@@ -1,5 +1,30 @@
 function love.load()
-
+    local RWidth, RHeight = love.window.getDesktopDimensions()
+    love.window.setMode(RWidth, RHeight - 60)
+    love.window.setTitle("Rock Out")
+    guitar = {
+        sprite = love.graphics.newImage('sprites/RedElectricGuitar.png'),
+        width = 0.5,
+        height = 0.5,
+        rotation = 0.25,
+        reward = 1
+    }
+    guitar.ObjW = guitar.sprite:getWidth() * guitar.width * guitar.rotation
+    guitar.ObjH = guitar.sprite:getHeight() * guitar.height
+    local SWidth, SHeight = love.graphics.getWidth(), love.graphics.getHeight()
+    local Xoffset = 200
+    local Yoffset = 150
+    guitar.x = (SWidth - guitar.ObjW - Xoffset) / 2
+    guitar.y = (SHeight - guitar.ObjH - Yoffset) / 2
+end
+local fans = 0
+function love.mousepressed(mx, my, button)
+    if button == 1 then
+        if mx >= guitar.x and mx <= guitar.x + guitar.ObjW and
+        my >= guitar.y and my <= guitar.y + guitar.ObjH then
+            fans = fans + guitar.reward
+        end
+    end
 end
 
 function love.update(dt)
@@ -7,5 +32,6 @@ function love.update(dt)
 end
 
 function love.draw()
-
+    love.graphics.draw(guitar.sprite, guitar.x, guitar.y, guitar.rotation, guitar.width, guitar.height)
+    love.graphics.print(fans)
 end
